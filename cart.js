ad01a51ce5667432e85964a022d5c85fd455285f
Maksim Galun <maxgalun@gmail.com>
2022-03-03 01:06:33 +0300

    Changes to be committed: modified:   cart.js

--- cart.js
+++ cart.js
@@ -17,7 +17,7 @@ function setlocalStorageCartCounter() {
 }
 
 async function getCatalog() {
-  let response = await fetch(
+  const response = await fetch(
     "https://raw.githubusercontent.com/maxgalun/classes/master/shop-data/data.json"
   );
   let json = await response.json();
@@ -28,28 +28,39 @@ async function getCatalog() {
 function createNodeArrayCatalogLI(json) {
   let catalog = [];
   for (let i = 0; i < json.length; i++) {
-    let book = document.createElement("li");
-    let bookContent = document.createElement("div");
-    let bookPicture = document.createElement("img");
-    let bookTitle = document.createElement("div");
-    let bookButton = document.createElement("div");
-    book.classList.add("catalog__list-item", "book");
-    bookContent.classList.add("book__content");
-    bookPicture.classList.add("book__picture");
-    bookPicture.width = "153";
-    bookPicture.height = "258";
+    let book = createNodeCatalogLI();
+    const bookPicture = book.querySelector(".book__picture");
+    const bookTitle = book.querySelector(".book__title");
     bookPicture.alt = json[i].title;
     bookPicture.src = json[i].image;
-    bookTitle.classList.add("book__title");
     bookTitle.innerText = json[i].title;
-    bookButton.classList.add("book__button", "button");
-    bookButton.innerText = "В корзину";
-    bookButton.onclick = incrementCartCounter;
-    bookContent.append(bookPicture);
-    bookContent.append(bookTitle);
-    bookContent.append(bookButton);
-    book.append(bookContent);
     catalog.push(book);
   }
   return catalog;
+}
+
+function createNodeCatalogLI() {
+  const bookPictureWidth = "153";
+  const bookPictureHeight = "258";
+  const bookButtonText = "В корзину";
+  const bookButtonOnClick = incrementCartCounter;
+  const book = document.createElement("li");
+  const bookContent = document.createElement("div");
+  const bookPicture = document.createElement("img");
+  const bookTitle = document.createElement("div");
+  const bookButton = document.createElement("div");
+  book.classList.add("catalog__list-item", "book");
+  bookContent.classList.add("book__content");
+  bookPicture.classList.add("book__picture");
+  bookTitle.classList.add("book__title");
+  bookButton.classList.add("book__button", "button");
+  bookPicture.width = bookPictureWidth;
+  bookPicture.height = bookPictureHeight;
+  bookButton.innerText = bookButtonText;
+  bookButton.onclick = bookButtonOnClick;
+  bookContent.append(bookPicture);
+  bookContent.append(bookTitle);
+  bookContent.append(bookButton);
+  book.append(bookContent);
+  return book;
 }
