13ac1e536ca41a32d8eb29f72fda36af835d428b
Maksim Galun <maxgalun@gmail.com>
2022-03-05 17:28:24 +0300

    Changes to be committed: modified:   cart.js

--- cart.js
+++ cart.js
@@ -1,7 +1,60 @@
 let catalog;
-getCatalog();
-let cart = JSON.parse(localStorage.getItem("cart"));
+let cart;
+fillCatalog();
+fillCart();
 updateCartCounterDOM();
+
+async function fillCatalog() {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/maxgalun/classes/master/shop-data/data.json"
+  );
+  catalog = await response.json();
+  createNodeArrayCatalogListItem();
+}
+
+function createNodeArrayCatalogListItem() {
+  let nodeArrayCatalogListItem = [];
+  const catalogList = document.querySelector(".catalog__list");
+  for (let i = 0; i < catalog.length; i++) {
+    let nodeCatalogListItem = createNodeCatalogListItem();
+    const bookPicture = nodeCatalogListItem.querySelector(".book__picture");
+    const bookTitle = nodeCatalogListItem.querySelector(".book__title");
+    const bookButton = nodeCatalogListItem.querySelector(".book__button");
+    bookPicture.alt = catalog[i].title;
+    bookPicture.src = catalog[i].image;
+    bookTitle.innerText = catalog[i].title;
+    bookButton.id = catalog[i].id;
+    nodeArrayCatalogListItem.push(nodeCatalogListItem);
+  }
+  catalogList.append(...nodeArrayCatalogListItem);
+}
+
+function createNodeCatalogListItem() {
+  const nodeCatalogListItem = document.createElement("li");
+  const bookContent = document.createElement("div");
+  const bookPicture = document.createElement("img");
+  const bookTitle = document.createElement("div");
+  const bookButton = document.createElement("div");
+  nodeCatalogListItem.classList.add("catalog__list-item", "book");
+  bookContent.classList.add("book__content");
+  bookPicture.classList.add("book__picture");
+  bookTitle.classList.add("book__title");
+  bookButton.classList.add("book__button", "button");
+  bookPicture.width = "153";
+  bookPicture.height = "258";
+  bookButton.innerText = "В корзину";
+  bookButton.onclick = addGoodsItemtoCart;
+  bookContent.append(bookPicture);
+  bookContent.append(bookTitle);
+  bookContent.append(bookButton);
+  nodeCatalogListItem.append(bookContent);
+  return nodeCatalogListItem;
+}
+
+function fillCart() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+  return;
+}
 
 function updateCartCounterDOM() {
   if (cart) {
@@ -28,51 +81,4 @@ function updateCart(goodsId) {
       cart.push(element);
     }
   });
-}
-
-async function getCatalog() {
-  const response = await fetch(
-    "https://raw.githubusercontent.com/maxgalun/classes/master/shop-data/data.json"
-  );
-  catalog = await response.json();
-  const catalogList = document.querySelector(".catalog__list");
-  catalogList.append(...createNodeArrayCatalogLI(catalog));
-}
-
-function createNodeArrayCatalogLI(catalog) {
-  let nodeArrayCatalogLI = [];
-  for (let i = 0; i < catalog.length; i++) {
-    let nodeCatalogLI = createNodeCatalogLI();
-    const bookPicture = nodeCatalogLI.querySelector(".book__picture");
-    const bookTitle = nodeCatalogLI.querySelector(".book__title");
-    const bookButton = nodeCatalogLI.querySelector(".book__button");
-    bookPicture.alt = catalog[i].title;
-    bookPicture.src = catalog[i].image;
-    bookTitle.innerText = catalog[i].title;
-    bookButton.id = catalog[i].id;
-    nodeArrayCatalogLI.push(nodeCatalogLI);
-  }
-  return nodeArrayCatalogLI;
-}
-
-function createNodeCatalogLI() {
-  const nodeCatalogLI = document.createElement("li");
-  const bookContent = document.createElement("div");
-  const bookPicture = document.createElement("img");
-  const bookTitle = document.createElement("div");
-  const bookButton = document.createElement("div");
-  nodeCatalogLI.classList.add("catalog__list-item", "book");
-  bookContent.classList.add("book__content");
-  bookPicture.classList.add("book__picture");
-  bookTitle.classList.add("book__title");
-  bookButton.classList.add("book__button", "button");
-  bookPicture.width = "153";
-  bookPicture.height = "258";
-  bookButton.innerText = "В корзину";
-  bookButton.onclick = addGoodsItemtoCart;
-  bookContent.append(bookPicture);
-  bookContent.append(bookTitle);
-  bookContent.append(bookButton);
-  nodeCatalogLI.append(bookContent);
-  return nodeCatalogLI;
 }
